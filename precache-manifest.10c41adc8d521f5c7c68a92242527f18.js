fe0fcafbb628f5e392a59c8570b43e005dca957c
youvovk <wer3wollf@gmail.com>
2019-12-22 20:41:02 +0200

    Updates

---
+++ precache-manifest.10c41adc8d521f5c7c68a92242527f18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d2a8c11b8397357bed947094d868ca",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "1b49c10edaf44935a13c",
+    "url": "/react_uber-eats/static/css/main.7c8cbf69.chunk.css"
+  },
+  {
+    "revision": "a3e728ec68e72a0d1231",
+    "url": "/react_uber-eats/static/js/2.cc45ae1d.chunk.js"
+  },
+  {
+    "revision": "1b49c10edaf44935a13c",
+    "url": "/react_uber-eats/static/js/main.4ff0c73b.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);

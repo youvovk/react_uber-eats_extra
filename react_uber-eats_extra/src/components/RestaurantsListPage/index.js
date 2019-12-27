f52e1ded1f89af016a874e127847fb71c8cf034b
youvovk <wer3wollf@gmail.com>
2019-12-27 14:48:34 +0200

    fix index

--- react_uber-eats_extra/src/components/RestaurantsListPage/index.js
+++ react_uber-eats_extra/src/components/RestaurantsListPage/index.js
@@ -1,5 +1,7 @@
 import { connect } from 'react-redux';
-import { RestaurantsListPage } from './RestaurantsListPage';
+import { 
+  RestaurantsListPage, 
+} from './RestaurantsListPage';
 import { 
   loadRestaurants, 
   loadLocations, 

ccb1b391dc79f3f2e4c9d157f8f57dd8b8a6e412
youvovk <wer3wollf@gmail.com>
2020-01-03 18:27:12 +0200

    fix index

--- react_uber-eats_extra/src/components/RestaurantsListPage/index.js
+++ react_uber-eats_extra/src/components/RestaurantsListPage/index.js
@@ -2,15 +2,18 @@ import { connect } from 'react-redux';
 import { 
   RestaurantsListPage, 
 } from './RestaurantsListPage';
+
 import { 
   loadRestaurants, 
   loadLocations, 
 } from '../../store/actions';
+
 import {
   selectorRestaurants,
   selectIsLoading,
   selectRestaurantsListError,
 } from '../../store/selectors';
+
 function mapState2Props(state) {
   return {
     restaurantsData: selectorRestaurants(state),
